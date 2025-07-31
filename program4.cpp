8c76153e9bd3ba5a734c73d0aa38c4e833c8e225
MHMD859 <mmmollo777@gmail.com>
2025-07-31 08:01:22 +0300

    Update program4.cpp

---
+++ program4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string addBinary(string a, string b) {
+    string result = "";
+    int i = a.length() - 1, j = b.length() - 1, carry = 0;
+    while(i >= 0 || j >= 0 || carry) {
+        int sum = carry;
+        if(i >= 0) sum += a[i--] - '0';
+        if(j >= 0) sum += b[j--] - '0';
+        result += (sum % 2) + '0';
+        carry = sum / 2;
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+int main() {
+    string a, b;
+    cout << "Enter first binary string: ";
+    cin >> a;
+    cout << "Enter second binary string: ";
+    cin >> b;
+    cout << "Sum: " << addBinary(a, b) << endl;
+    return 0;
+}
